c03bf8144efe1828bc2a8a72e45a61ea33fa65e7
Isaac Leeson <isaacleeson@proton.me>
2025-05-05 14:09:16 -0400

    update

--- src/DynamicProgrammingExamples.jl
+++ src/DynamicProgrammingExamples.jl
@@ -1,6 +1,7 @@
 module DynamicProgrammingExamples
 
 include("rbc.jl")
+include("rbc_precompute.jl")
 include("cake_eating_problem.jl")
 
 end
--- src/run.jl
+++ src/run.jl
@@ -1,10 +1,12 @@
-using Metal, BenchmarkTools, DataFrames
+using CUDA, BenchmarkTools, DataFrames, Adapt, FileIO, Dates, CSV
 import CommonSolve.solve!, CommonSolve.init
 
-import DynamicProgrammingExamples.CakeEatingProblem as CP
-import DynamicProgrammingExamples.RBC 
+include("DynamicProgrammingGPU.jl")
+using .DynamicProgrammingGPU
 
-function push_time!(t, name, n, nsteps)
+
+
+function push_time!(filename, t, date, name, n, nsteps)
     cpu = @belapsed(solve!(x, p),
         setup = (
             p = getproperty(eval($name), :Parameters)();
@@ -23,15 +25,20 @@ function push_time!(t, name, n, nsteps)
         setup = (
             p = getproperty(eval($name), :Parameters)();
             x = getproperty(eval($name), :init)(p,$n);
-            p_tex = cu(p);
-            x_tex = cu(x)
+            p_tex = cutex(p);
+            x_tex = cutex(x)
         )
     )
-    push!(t, (; name, n, cpu, gpu))
+    push!(t, (; date, name, n=string(n), cpu, gpu, tex))
+    CSV.write(filename, t)
 end
 
-t = DataFrame(name = Symbol[], n = Tuple[], cpu = Float64[], gpu = Float64[], tex = Float64[])
-[push_time!(t, name, (n,), 10^2) for n in 32 .* 2 .^ (0:5),
+filename, date = ("times.csv", now())
+types = Dict(1 => DateTime, 2 => Symbol, 4 => Float64, 5 => Float64, 6 => Float64)
+t = isfile(filename) ? CSV.read(filename, DataFrame; types) : DataFrame(
+    date = DateTime[], name = Symbol[], n = String[],
+    cpu = Float64[], gpu = Float64[], tex = Float64[]
+)
+[push_time!(filename, t, date, name, (n,), 10^2) for n in 32 .* 2 .^ (0:0),
     name in (:CP,)]
-[push_time!(t, name, (n,m), 10^2) for n in 32 .* 2 .^ (0:5), m in (8,),
-    name in (:RBC,)]
+[push_time!(filename, t, date, name, (n,m), 10^2) for n in 32 .* 2 .^ (0:0), m in (8,), name in (:RBC_QUADRATURE,)]
